65571ba151b4d82f5f3854fbe85d46564e050852
kmorin124 <35405619+kmorin124@users.noreply.github.com>
2019-07-14 14:54:26 -0500

    Init

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +9,72 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  items = [
+    {
+      name: "Milk",
+      quantity: 2
+    },
+    {
+      name: "Butter",
+      quantity: 4
+    },    
+    {
+      name: "Ice Cream",
+      quantity: 3
+    }
+  ];
+
+  constructor(public toastController: ToastController, public alertController: AlertController) {}
+
+  async removeItem(item, index) {
+    console.log("Removing Item - ", index, item)
+    const toast = await this.toastController.create({
+      message: 'Removing ' + item.name + "...",
+      duration: 3000
+    });
+    toast.present();
+    this.items.splice(index, 1)
+  }
+
+  async addItem() {
+    console.log("Adding Item -");
+    this.addItemPrompt()
+  }
+
+  async addItemPrompt() {
+    const alert = await this.alertController.create({
+      header: 'Add grocery item...',
+      inputs: [
+        {
+          name: 'name',
+          type: 'text',
+          placeholder: 'Item'
+        },
+        {
+          name: 'quantity',
+          type: 'number',
+          placeholder: 'Quantity'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        }, {
+          text: 'Ok',
+          handler: item => {
+            console.log('Item added -', item);
+            this.items.push(item)
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
 
 }
